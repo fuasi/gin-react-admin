2e677c378537473c506737ce428202de0ff89da1
Lank <1120236287@qq.com>
2023-06-19 17:06:46 +0800

    完成数据库和封装请求

--- server/app.go
+++ server/app.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
-	"server/apis"
 	"server/initialize"
 	"server/middlewares"
 )
@@ -12,7 +11,6 @@ func main() {
 	initialize.InitEnter()
 	server := gin.Default()
 	middlewares.InitMiddleware(server)
-	apis.InitApis()
 	err := server.Run(":8088")
 	if err != nil {
 		log.Fatalln(err.Error())
