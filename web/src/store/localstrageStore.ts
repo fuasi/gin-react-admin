c890421dd104c781bc607e8b52bee0e4b2ae1233
Kar <438577872@qq.com>
2023-07-10 17:00:46 +0800

    fix: change token utils to store

--- web/src/store/localstrageStore.ts
+++ web/src/store/localstrageStore.ts
@@ -1,12 +1,31 @@
 import { observable, autorun } from 'mobx'
-import { deleteToken, getToken, setToken } from '@/utils/token'
+
+const tokenKey = 'tokenKey'
+const setToken = (token: string) => {
+    return localStorage.setItem(tokenKey, token)
+}
+
+const getToken = (): string => {
+    return localStorage.getItem(tokenKey) || ''
+}
+
+const deleteToken = () => {
+    return localStorage.removeItem(tokenKey)
+}
 
 const tokenStore = observable({
-    token : getToken(),
+    token: getToken(),
 }, {})
 
+window.addEventListener('storage', e => {
+    if (e.key == tokenKey) {
+        tokenStore.token = e.newValue
+    }
+})
+
+
 autorun(() => {
-    if ( tokenStore.token ) {
+    if (tokenStore.token) {
         setToken(tokenStore.token)
     } else {
         deleteToken()
