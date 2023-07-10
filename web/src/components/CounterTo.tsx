68c097dcaffd8d28a1c51ecb4ae6b179f7b5caf2
Kar <438577872@qq.com>
2023-07-10 17:26:09 +0800

    todo: Component "<CounterTo/>" is uncompleted

--- web/src/components/CounterTo.tsx
+++ web/src/components/CounterTo.tsx
@@ -1,12 +1,28 @@
 // todo: 我tm直接开摆
-import { useState } from 'react'
+import { ReactComponentElement } from 'react'
+/*
+    <CounterTo count={30}>
+        {
+            (count)=>{
+                return <div>{count}</div>
+            }
+        }
+    </CounterTo>
+ */
+type CounterToPropsType<T = number> = {
+    value: T,
+    duration: number,
+    delay: number,
+    children: (count: T) => ReactComponentElement<any>,
+    initValue: T
+}
+export const CounterTo = ({ value, duration = .3, delay = 0, children, initValue }: CounterToPropsType) => {
 
-export const CounterTo = (
-    { count, duration = .3, delay = 0 }:
-        { count: number, duration: number, delay: number }
-) => {
+    if (children) {
+        return children(value)
+    }
 
     return <div>
-        { count }
+        { value }
     </div>
 }
