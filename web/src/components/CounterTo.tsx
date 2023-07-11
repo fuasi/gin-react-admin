0770952f663d510655b7d9e2182f1ec5918a8bbe
Lank <1120236287@qq.com>
2023-07-11 16:07:58 +0800

    feat:complete the counter assembly

--- web/src/components/CounterTo.tsx
+++ web/src/components/CounterTo.tsx
@@ -1,5 +1,5 @@
-// todo: 我tm直接开摆
-import { ReactComponentElement } from 'react'
+import Styles from './Counter.module.scss'
+import { useEffect, useState } from "react";
 /*
     <CounterTo count={30}>
         {
@@ -9,20 +9,36 @@ import { ReactComponentElement } from 'react'
         }
     </CounterTo>
  */
+
 type CounterToPropsType<T = number> = {
     value: T,
     duration: number,
     delay: number,
-    children: (count: T) => ReactComponentElement<any>,
     initValue: T
 }
-export const CounterTo = ({ value, duration = .3, delay = 0, children, initValue }: CounterToPropsType) => {
 
-    if (children) {
-        return children(value)
+export const CounterTo = ({ value, duration = .3, delay = 0, initValue }: CounterToPropsType) => {
+    const [counterRun, setCounterRun] = useState<boolean>(false)
+
+    const initCounterStyle = {
+        transition : `--counterValue ${duration}ms ${delay}ms`,
+        '--counterValue' : initValue
     }
 
+    const counterStyle = {
+        transition : `--counterValue ${duration}ms ${delay}ms`,
+        '--counterValue' : value
+    }
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCounterRun(true)
+        }, 0)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [])
+
     return <div>
-        { value }
+        <span style={counterRun ? counterStyle : initCounterStyle} className={`${Styles.counter} text-4xl`}></span>
     </div>
 }
