7e962b9931dc71d0f9688ac044fbffa458f832b0
Kar <438577872@qq.com>
2023-07-12 22:45:46 +0800

    chore: simplify any code

--- web/src/views/backend/backendLayout.tsx
+++ web/src/views/backend/backendLayout.tsx
@@ -1,48 +1,49 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { Layout, theme } from "antd";
-import { useState } from "react";
-import SideMenuComponent from "./components/SideMenuComponent.tsx";
-import { BreadcrumbItemType, BreadcrumbSeparatorType } from "antd/es/breadcrumb/Breadcrumb";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
+import { Outlet, useLocation } from 'react-router-dom';
+import { Layout, theme } from 'antd';
+import { useState } from 'react';
+import SideMenuComponent from './components/SideMenuComponent.tsx';
+import { BreadcrumbItemType, BreadcrumbSeparatorType } from 'antd/es/breadcrumb/Breadcrumb';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import './backendLayout.scss'
-import HeaderComponent from "@/views/backend/components/HeaderComponent.tsx";
-import { useForceUpdate } from "@/hooks/useForceUpdate.ts";
+import HeaderComponent from '@/views/backend/components/HeaderComponent.tsx';
+import { useKeyCounter } from '@/hooks/useKeyCounter'
 
 const { Sider } = Layout;
 
 const BackendLayout = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const [breadcrumb, setBreadcrumb] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] | undefined>([])
-    const { token : { colorBgContainer } } = theme.useToken();
+    const [ collapsed, setCollapsed ] = useState(false);
+    const [ breadcrumb, setBreadcrumb ] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] | undefined>([])
+    const { token: { colorBgContainer } } = theme.useToken();
     const location = useLocation()
-    const [forceUpdate, count] = useForceUpdate()
+    const { key, incr } = useKeyCounter()
+
+    // const [forceUpdate, count] = useForceUpdate()
 
     return (
-        <Layout className={"w-screen h-screen"}>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                {/*<div className="demo-logo-vertical">*/}
-                {/*    logooo*/}
-                {/*</div>*/}
-                <SideMenuComponent setBreadcrumb={setBreadcrumb}/>
+        <Layout className={ 'w-screen h-screen' }>
+            <Sider trigger={ null } collapsible collapsed={ collapsed }>
+                {/*<div className="demo-logo-vertical">*/ }
+                {/*    logooo*/ }
+                {/*</div>*/ }
+                <SideMenuComponent setBreadcrumb={ setBreadcrumb }/>
             </Sider>
             <Layout>
-                <HeaderComponent forceUpdate={forceUpdate} setCollapsed={setCollapsed} breadcrumb={breadcrumb}
-                                 colorBgContainer={colorBgContainer}
-                                 collapsed={collapsed}/>
+                <HeaderComponent forceUpdate={ incr } setCollapsed={ setCollapsed } breadcrumb={ breadcrumb }
+                                 colorBgContainer={ colorBgContainer }
+                                 collapsed={ collapsed }/>
                 <SwitchTransition mode="out-in">
                     <CSSTransition
-                        unmountOnExit={true}
-                        key={location.key}
-                        timeout={300} classNames="fade" nodeRef={null}>
-                        <div key={count} className={"overflow-auto"}>
+                        unmountOnExit={ true }
+                        key={ location.key }
+                        timeout={ 300 } classNames="fade" nodeRef={ null }>
+                        <div key={ key } className={ 'overflow-auto' }>
                             <Outlet/>
                         </div>
                     </CSSTransition>
                 </SwitchTransition>
             </Layout>
         </Layout>
-    )
-        ;
+    );
 }
 
 export default BackendLayout
