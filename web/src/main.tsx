a35b654af4caf4610b293c3bb2b938cd7d0c907f
Lank <1120236287@qq.com>
2023-07-12 09:29:56 +0800

    chore: enable react strict mode

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,13 @@ import App from './App.tsx'
 import './index.scss'
 import 'nprogress/nprogress.css';
 import { configure } from "mobx";
+import { StrictMode } from "react";
 
 configure({
     enforceActions : "never"
 })
 createRoot(document.getElementById('root') as HTMLElement).render(
-    <App/>
+    <StrictMode>
+        <App/>
+    </StrictMode>
 )
