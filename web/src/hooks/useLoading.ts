405da53cd401148435ec7a277fda0ecc3f5fa4a0
Kar <438577872@qq.com>
2023-07-13 20:24:13 +0800

    fix: rollback last fix

--- web/src/hooks/useLoading.ts
+++ web/src/hooks/useLoading.ts
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
-export const useLoading = () => {
-    const [ loading, setLoading ] = useState(false)
+// fix nm
+export const useLoading = (initLoading = false) => {
+    const [ loading, setLoading ] = useState(initLoading)
     const withLoading = async (func: () => void) => {
         setLoading(true)
         try {
--- web/src/views/backend/admin/user/index.tsx
+++ web/src/views/backend/admin/user/index.tsx
@@ -12,7 +12,7 @@ import { useLoading } from '@/hooks/useLoading'
 const UserComponent = () => {
     const [ selectedRowKeys, setSelectedRowKeys ] = useState<React.Key[]>([]);
     const [ pageInfo, setPageInfo ] = useState<PageInfo>({ pageSize: 10, page: 1 })
-    const { loading, withLoading } = useLoading()
+    const { loading, withLoading } = useLoading(true)
     const [ data, setDate ] = useState<User[]>([])
     useEffect(() => {
         withLoading(() => {
@@ -64,13 +64,15 @@ const UserComponent = () => {
             title: '操作',
             key: 'action',
             width: 256,
-            render: (_, record) => (<Space size="middle">
-                <a onClick={ () => handleUpdateUser(record) }><EditOutlined className={ 'mr-2' }/>编辑</a>
+            render: (_, record) => (
+                <Space size="middle">
+                    <a onClick={ () => handleUpdateUser(record) }><EditOutlined className={ 'mr-2' }/>编辑</a>
 
-                <a onClick={ () => handleDeleteUser(record) }><DeleteOutlined className={ 'mr-2' }/>删除</a>
+                    <a onClick={ () => handleDeleteUser(record) }><DeleteOutlined className={ 'mr-2' }/>删除</a>
 
-                <a onClick={ () => handleResetPassword(record) }><KeyOutlined className={ 'mr-2' }/>重置密码</a>
-            </Space>)
+                    <a onClick={ () => handleResetPassword(record) }><KeyOutlined className={ 'mr-2' }/>重置密码</a>
+                </Space>
+            )
         },
     ];
     const handleUpdateUser = (user: User) => {
