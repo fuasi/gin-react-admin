6fea072d9d4e52e196de6f529419e3aa9d3e78a8
fuasi <1120236287@qq.com>
2023-08-01 09:04:26 +0800

    test：upload code test for new username and email

--- server/app.go
+++ server/app.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"server/initialize"
+	"server/middlewares"
 )
 
 func main() {
 	server := initialize.InitEnter()
-	//middlewares.InitMiddleware(server)
+	middlewares.InitMiddleware(server)
 	err := server.Run(":8888")
 	if err != nil {
 		log.Fatalln(err.Error())
