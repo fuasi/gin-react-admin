ecdcccebc6adf3e90b36ab26b54f4e61770b17c4
fuasi <h1120236287@outlook.com>
2023-08-01 17:25:18 +0800

    feat: page timeout Jump to login

--- web/src/components/RouterLoadingComponent.tsx
+++ web/src/components/RouterLoadingComponent.tsx
@@ -1,9 +1,25 @@
-import {Spin} from "antd";
+import { message, Spin } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const RouterLoadingComponent = () => {
+    const [messageApi, contextHolder] = message.useMessage()
+    const navigate = useNavigate()
+    useEffect(() => {
+        setTimeout(() => {
+            messageApi.open({
+                type : "success",
+                content : "加载超时,即将跳转到登录页面,请重新进行登录!",
+                duration : 2
+            }).then(() => {
+                navigate("/login")
+            })
+        }, 10000)
+    }, [])
     return (
         <div className={"flex justify-center items-center"}>
-            <Spin tip="正在加载页面中......" style={{position: "fixed"}} size="large">
+            {contextHolder}
+            <Spin tip="正在加载页面中......" style={{ position : "fixed" }} size="large">
                 <div className="content"/>
             </Spin>
         </div>
