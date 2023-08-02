bae387e261554ffd480ab320935ae7e698f096ad
fuasi <h1120236287@outlook.com>
2023-08-02 17:22:38 +0800

    styles: config update

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    corePlugins: {
-        preflight: false
+    content : [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}", ], corePlugins : {
+        preflight : false
     },
-    theme: {
-        extend: {},
+    theme : {
+        extend : {},
     },
-    plugins: [],
+    plugins : [],
 }
