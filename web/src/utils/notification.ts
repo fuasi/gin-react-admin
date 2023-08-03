84beab5080c8215e9b7059f8d5cb8f77ff9b2555
fuasi <h1120236287@outlook.com>
2023-08-03 17:21:11 +0800

    perf: optimized the writing of the code

--- web/src/utils/notification.ts
+++ web/src/utils/notification.ts
@@ -1,35 +1,36 @@
 import { notificationStorage } from "@/store/notificationStorage.ts";
 import { GLOBAL_NOTIFICATION_TEXT } from "@/config";
+import { IconType } from "antd/es/notification/interface";
 
 export const notificationLoginSuccess = (description: string) => {
-    notificationStorage.globalNotification = {
-        type : "success",
-        message : GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_LOGIN_SUCCESS,
-        description : description
-    }
-    notificationStorage.type = "active"
+    notificationSuccess(GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_LOGIN_SUCCESS, description)
 }
 export const notificationLoginFail = (description: string) => {
-    notificationStorage.globalNotification = {
-        type : "error",
-        message : GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_LOGIN_FAIL,
-        description : description
-    }
-    notificationStorage.type = "active"
+    notificationFail(GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_LOGIN_FAIL, description)
+}
+export const notificationLogout = (description: string) => {
+    notificationSuccess(GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_ACTIVE_LOGOUT, description)
 }
 export const notificationActiveSuccess = (description: string) => {
-    notificationStorage.globalNotification = {
-        type : "success",
-        message : GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_ACTIVE_SUCCESS,
-        description : description
-    }
-    notificationStorage.type = "active"
+    notificationSuccess(GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_SYSTEM, description)
 }
 export const notificationActiveFail = (description: string) => {
+    notificationFail(GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_SYSTEM, description)
+}
+
+const notificationSuccess = (message: string, description: string) => {
+    baseNotification("success", message, description)
+}
+const notificationFail = (message: string, description: string) => {
+    baseNotification("error", message, description)
+}
+
+const baseNotification = (type: IconType, message: string, description: string) => {
     notificationStorage.globalNotification = {
-        type : "error",
-        message : GLOBAL_NOTIFICATION_TEXT.NOTIFICATION_MESSAGE_ACTIVE_FAIL,
-        description : description
+        type : type,
+        message : message,
+        description : description,
+        duration : 2,
     }
     notificationStorage.type = "active"
 }
