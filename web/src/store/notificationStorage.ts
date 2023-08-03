4308cb2c1bdb4058931b3ed0c8179433ee7429be
fuasi <h1120236287@outlook.com>
2023-08-03 17:21:33 +0800

    feat: added custom duration

--- web/src/store/notificationStorage.ts
+++ web/src/store/notificationStorage.ts
@@ -2,11 +2,12 @@ import { observable } from "mobx";
 import { ArgsProps } from "antd/es/notification/interface";
 
 
-const notificationStorage = observable<{globalNotification: ArgsProps, type: "sleep" | "active"}>({
+const notificationStorage = observable<{ globalNotification: ArgsProps, type: "sleep" | "active" }>({
     globalNotification : {
         type : "success",
         message : "",
-        description : ""
+        description : "",
+        duration : null
     },
     type : "sleep"
 }, {})
