dade38a4ca9fe454ab81ed81884746b101d3f763
fuasi <h1120236287@outlook.com>
2023-08-03 17:18:03 +0800

    fixed: repeated loading caused by "SwitchTransition"

--- web/src/views/backend/backendLayout.tsx
+++ web/src/views/backend/backendLayout.tsx
@@ -1,9 +1,8 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Layout, theme } from 'antd';
 import { useState } from 'react';
 import SideMenuComponent from './components/SideMenuComponent.tsx';
 import { BreadcrumbItemType, BreadcrumbSeparatorType } from 'antd/es/breadcrumb/Breadcrumb';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import './backendLayout.scss'
 import HeaderComponent from '@/views/backend/components/HeaderComponent.tsx';
 import { useKeyCounter } from '@/hooks/useKeyCounter'
@@ -11,14 +10,10 @@ import { useKeyCounter } from '@/hooks/useKeyCounter'
 const { Sider } = Layout;
 
 const BackendLayout = () => {
-    const [ collapsed, setCollapsed ] = useState(false);
-    const [ breadcrumb, setBreadcrumb ] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] | undefined>([])
-    const { token: { colorBgContainer } } = theme.useToken();
-    const location = useLocation()
+    const [collapsed, setCollapsed] = useState(false);
+    const [breadcrumb, setBreadcrumb] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] | undefined>([])
+    const { token : { colorBgContainer } } = theme.useToken();
     const { key, incr } = useKeyCounter()
-
-    // const [forceUpdate, count] = useForceUpdate()
-
     return (
         <Layout className={ 'w-screen h-screen' }>
             <Sider trigger={ null }
@@ -30,25 +25,16 @@ const BackendLayout = () => {
                 {/*</div>*/ }
                 <SideMenuComponent setBreadcrumb={ setBreadcrumb }/>
             </Sider>
-            <Layout>
+            <Layout className={ "relative" }>
                 <HeaderComponent handleFunc={ incr }
                                  setCollapsed={ setCollapsed }
                                  breadcrumb={ breadcrumb }
                                  colorBgContainer={ colorBgContainer }
                                  collapsed={ collapsed }/>
-                <SwitchTransition mode="out-in">
-                    <CSSTransition
-                        unmountOnExit={ true }
-                        key={ location.key }
-                        timeout={ 300 }
-                        classNames="fade"
-                        nodeRef={ null }>
-                        <div key={ key }
-                             className={ 'overflow-auto' }>
-                            <Outlet/>
-                        </div>
-                    </CSSTransition>
-                </SwitchTransition>
+                <div key={ key }
+                     className={ 'overflow-auto' }>
+                    <Outlet/>
+                </div>
             </Layout>
         </Layout>
     );
