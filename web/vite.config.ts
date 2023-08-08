f101b1b4a21a262dce13849227d84d153deea885
fuasi <h1120236287@outlook.com>
2023-08-08 17:26:12 +0800

    feat：adding vite proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as http from "http";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ react(), splitVendorChunkPlugin() ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins : [react(), splitVendorChunkPlugin()],
+  resolve : {
+    alias : {
+      '@' : fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server : {
+    proxy : {
+      '/api' : {
+        target : 'http://127.0.0.1:8888',
+        changeOrigin : true,
+        rewrite : (path) => path.replace(/^\/api/, "api")
+      },
+      '/static' : {
+        target : 'http://127.0.0.1:5000',
+        changeOrigin : true,
+        rewrite : (path) => path.replace(/^\/static/, "static")
+      }
+    }
+  }
 })
