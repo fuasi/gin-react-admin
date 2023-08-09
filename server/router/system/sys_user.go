12760573c1e0ee396995c7ea829aac957b1026aa
fuasi <h1120236287@outlook.com>
2023-08-09 17:32:55 +0800

    feat:：remove form user to users

--- server/router/system/sys_user.go
+++ server/router/system/sys_user.go
@@ -14,13 +14,14 @@ func (u *UserRouter) InitUserRouter(Router *gin.RouterGroup) {
 		Router.POST("/login", apis.SystemApisApp.SystemApis.UserApi.Login)
 	}
 	userRouter := Router.Use(middlewares.AuthMiddleware)
+	//userRouter := Router.Use()
 	userApi := apis.SystemApisApp.SystemApis.UserApi
 	{
 		userRouter.POST("/check", apis.SystemApisApp.SystemApis.UserApi.CheckLogin)
 		userRouter.POST("/users", userApi.GetUserList)
 		userRouter.GET("/user/:id", userApi.GetUserById)
 		userRouter.PATCH("/user", userApi.UpdateUserById)
-		userRouter.DELETE("/user/:id", userApi.DeleteUserById)
+		userRouter.DELETE("/user", userApi.DeleteUserById)
 		userRouter.PUT("/user", userApi.InsertUser)
 		userRouter.PATCH("/user/:id", userApi.ResetUserPassword)
 	}
