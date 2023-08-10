49086fc4a931b3f97d44d65a53c40104d26e2cbb
fuasi <h1120236287@outlook.com>
2023-08-10 17:27:01 +0800

    styles：updated the login parameter types

--- web/src/apis/baseApis.ts
+++ web/src/apis/baseApis.ts
@@ -1,44 +1,45 @@
 import { apiRequest } from './index.ts';
+import { User } from "@/apis/userApis.ts";
 
 export interface PageInfo {
-    pageSize: number
-    page: number
+  pageSize : number
+  page : number
 }
 
-interface LoginQuery {
-    username: string
-    password: string
+export interface LoginQuery {
+  username : string
+  password : string
 }
 
 export interface RouterResponse {
-    id: number
-    name: string
-    icon: string
-    path: string
-    componentPath: string
-    parentId: number
-    routerOrder: number
-    children?: RouterResponse[]
+  id : number
+  name : string
+  icon : string
+  path : string
+  componentPath : string
+  parentId : number
+  routerOrder : number
+  children? : RouterResponse[]
 }
 
-export function login(query: LoginQuery) {
-    return apiRequest<string>({
-        url : '/login',
-        method : 'POST',
-        data : query
-    })
+export function login(query : LoginQuery) {
+  return apiRequest<{ user : User, token : string }>({
+    url : '/login',
+    method : 'POST',
+    data : query
+  })
 }
 
 export function checkLogin() {
-    return apiRequest<string>({
-        url : '/check',
-        method : 'POST',
-    })
+  return apiRequest<string>({
+    url : '/check',
+    method : 'POST',
+  })
 }
 
 export function getRouter() {
-    return apiRequest<RouterResponse[]>({
-        url : '/router',
-        method : 'GET'
-    })
+  return apiRequest<RouterResponse[]>({
+    url : '/router',
+    method : 'GET'
+  })
 }
