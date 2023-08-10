3f7e2b5ae442d9395895b99df4cf598f471ce93a
fuasi <h1120236287@outlook.com>
2023-08-10 17:29:18 +0800

    feat：enable the blocking function and condition search

--- server/service/system/sys_user.go
+++ server/service/system/sys_user.go
@@ -3,9 +3,9 @@ package system
 import (
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
-	"server/global"
-	"server/models/common/request"
+	global "server/global"
 	"server/models/system"
+	"server/models/system/request"
 	"server/utils"
 )
 
@@ -14,15 +14,17 @@ type UserService struct {
 
 func (UserService *UserService) Login(u system.SysUser) (resultUser system.SysUser, err error) {
 	var user system.SysUser
-
 	if errors.Is(global.GRA_DB.Where("username = ?", u.Username).First(&user).Error, gorm.ErrRecordNotFound) {
 		return resultUser, errors.New("账号或密码错误")
+	}
+	if user.Enable == -1 {
+		return resultUser, errors.New("账号已被禁用,请联系管理员启用!")
 	}
 	login := utils.VerifyPassword(user.Password, u.Password)
 	if !login {
 		return resultUser, errors.New("账号或密码错误")
 	}
-	return u, err
+	return user, err
 }
 
 func (UserService *UserService) GetUserById(u system.SysUser) (resultUser system.SysUser, err error) {
@@ -43,19 +45,25 @@ func (UserService *UserService) UpdateUserById(u system.SysUser) error {
 		}).Error
 }
 
-func (UserService *UserService) DeleteUserById(u system.SysUser) error {
-	return global.GRA_DB.Delete(u).Error
-
+func (UserService *UserService) DeleteUserByIds(ids []uint) error {
+	return global.GRA_DB.Delete(system.SysUser{}, ids).Error
 }
 
-func (UserService *UserService) GetUserList(info request.PageInfo) (resultUser []system.SysUserPublic, total int64, err error) {
-	limit := info.PageSize
-	offset := (info.Page - 1) * info.PageSize
-	err = global.GRA_DB.Model(resultUser).Count(&total).Error
+func (UserService *UserService) GetUserList(request request.SearchUser) (resultUser []system.SysUserPublic, total int64, err error) {
+	limit := request.PageSize
+	offset := (request.Page - 1) * request.PageSize
+	tx := global.GRA_DB.Debug().Model(system.SysUser{}).Scopes(
+		utils.SearchWhere("id", request.Id, false),
+		utils.SearchWhere("username", request.Username, true),
+		utils.SearchWhere("nickname", request.Nickname, true),
+		utils.SearchWhere("phone", request.Phone, true),
+		utils.SearchWhere("enable", request.Enable, false),
+	)
+	err = tx.Count(&total).Error
 	if err != nil {
 		return
 	}
-	err = global.GRA_DB.Order("id").Limit(limit).Offset(offset).Find(&resultUser).Error
+	err = tx.Order("id").Limit(limit).Offset(offset).Find(&resultUser).Error
 	return resultUser, total, err
 }
 
