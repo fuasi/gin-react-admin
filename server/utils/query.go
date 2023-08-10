60f33e32ae496fa6ea0d5b9cc46c359f367c28f4
fuasi <h1120236287@outlook.com>
2023-08-10 17:28:45 +0800

    styles：the conditional query tool is packaged

---
+++ server/utils/query.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"reflect"
+)
+
+func SearchWhere(query string, args any, isLike bool) func(tx *gorm.DB) *gorm.DB {
+	return func(tx *gorm.DB) *gorm.DB {
+		if IsBlank(args) {
+			return tx
+		}
+		if isLike {
+			return tx.Where(fmt.Sprintf("%s like ?", query), "%"+args.(string)+"%")
+		}
+		return tx.Where(fmt.Sprintf("%s = ?", query), args)
+
+	}
+}
+func likeQuery(keyword string) string {
+	return "%" + keyword + "%"
+}
+
+func IsBlank(i any) bool {
+	value := reflect.ValueOf(i)
+	switch value.Kind() {
+	case reflect.String:
+		return value.Len() == 0
+	case reflect.Bool:
+		return !value.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return value.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return value.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return value.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return value.IsNil()
+	}
+	return reflect.DeepEqual(value.Interface(), reflect.Zero(value.Type()).Interface())
+}
