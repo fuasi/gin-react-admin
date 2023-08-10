25ab87e26da9d093baeeab9bc82947ba8d759382
fuasi <h1120236287@outlook.com>
2023-08-10 17:29:47 +0800

    feat：default to "/dashboard"

--- web/src/components/PrivateRouteComponent.tsx
+++ web/src/components/PrivateRouteComponent.tsx
@@ -1,11 +1,13 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { tokenStore } from '@/store/localstrageStore'
 
 
-const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-    if (!tokenStore.token) {
-        return <Navigate replace to={ '/login' }/>
-    }
-    return children
+const PrivateRoute = ({ children } : { children : React.ReactNode }) => {
+  const location = useLocation()
+  if (!tokenStore.token) {
+    return <Navigate replace to={ '/login' }/>
+  }
+  if (location.pathname === '/') return <Navigate to={ '/dashboard' }/>
+  return children
 }
 export default PrivateRoute
