75f4602a332e6856a074d33eafd9188279d8706a
fuasi <h1120236287@outlook.com>
2023-09-12 17:04:07 +0800

    fixed：react18 warning

--- web/src/components/RouterLoading.tsx
+++ web/src/components/RouterLoading.tsx
@@ -3,27 +3,30 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const RouterLoading = () => {
-    const [messageApi, contextHolder] = message.useMessage()
-    const navigate = useNavigate()
-    useEffect(() => {
-        setTimeout(() => {
-            messageApi.open({
-                type : "success",
-                content : "加载超时,即将跳转到登录页面,请重新进行登录!",
-                duration : 2
-            }).then(() => {
-                navigate("/login")
-            })
-        }, 10000)
-    }, [])
-    return (
-        <div className={"flex justify-center items-center"}>
-            {contextHolder}
-            <Spin tip="正在加载页面中......" style={{ position : "fixed" }} size="large">
-                <div className="content"/>
-            </Spin>
-        </div>
-    )
+  const [messageApi, contextHolder] = message.useMessage()
+  const navigate = useNavigate()
+  useEffect(() => {
+    const timeAlert = setTimeout(() => {
+      messageApi.open({
+        type : "success",
+        content : "加载超时,即将跳转到登录页面,请重新进行登录!",
+        duration : 2
+      }).then(() => {
+        navigate("/login")
+      })
+    }, 10000)
+    return () => {
+      clearTimeout(timeAlert)
+    }
+  }, [])
+  return (
+    <div className={ "flex justify-center items-center" }>
+      { contextHolder }
+      <Spin tip="正在加载页面中......" style={ { position : "fixed" } } size="large">
+        <div className="content"/>
+      </Spin>
+    </div>
+  )
 }
 
 
